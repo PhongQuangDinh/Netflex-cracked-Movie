a7abeb642ca0193a33a5d63055a1fa2ea3eaa002
PhongGodFather <dinhquangphong365@gmail.com>
2023-11-02 03:06:14 +0700

    SERIES AND MOVIE YEE

--- MovieDB_apiTest.py
+++ MovieDB_apiTest.py
@@ -8,14 +8,31 @@ account_id = 20574151
 #   url = f""
 #   # Return the custom redirect link.
 #   return custom_redirect_link.short_url
+def NotAdult(movie: dict):
+  ban = ['hentai','virgin','nude','fuck','dick', 'sex', 'flower & snake', 'hot night', 'unexpected attraction']
+  overview = movie['overview'].lower()
+  title = ''
+  if 'title' in movie:
+    title = movie['title'].lower()
+  for warning in ban:
+    if warning.lower() in overview or warning.lower() in title:
+        return False
+  return True
 
-def fetch_movie_data(movie_id, type_call = 'movie'):
+def fetch_video(movie_id, type_call = 'movie'):
+  url = f""
+
+def fetch_movie_data(movie_id):
   url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}"
   # url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={TMDB_API_KEY}"
   if type(movie_id) == str and not movie_id.isdigit():
     movie_id = movie_id.replace(' ','+')
     # print(movie_id)
-    url = f"https://api.themoviedb.org/3/search/movie?query={movie_id}&api_key={TMDB_API_KEY}"
+    # url = f"https://api.themoviedb.org/3/search/movie?query={movie_id}&include_adult=&api_key={TMDB_API_KEY}" # avoid adult content, but not work idk why
+    
+    url = f"https://api.themoviedb.org/3/search/movie?query={movie_id}&api_key={TMDB_API_KEY}" # with adult content also
+    # url = f"https://api.themoviedb.org/3/search/tv?query={movie_id}&include_adult=false&api_key={TMDB_API_KEY}"
+    
     # response = requests.get(url)
     # movie_data = response.json()
     # print(movie_data["title"])
@@ -25,7 +42,33 @@ def fetch_movie_data(movie_id, type_call = 'movie'):
   response = requests.get(url)
   # rep = requests.get(f"https://api.themoviedb.org/3/account/{account_id}")
   movie_data = response.json()
-  # print(movie_data["title"])
+  if 'results' in movie_data:
+    # far more better but worst in some case
+    movie_data['results'] = list(filter(lambda result: result['adult'] == False and NotAdult(result), movie_data['results']))
+  return movie_data
+
+def fetch_series_data(series_id):
+  url = f"https://api.themoviedb.org/3/tv/{series_id}?api_key={TMDB_API_KEY}"
+  # url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={TMDB_API_KEY}"
+  if type(series_id) == str and not series_id.isdigit():
+    series_id = series_id.replace(' ','+')
+    # print(movie_id)
+    # url = f"https://api.themoviedb.org/3/search/movie?query={movie_id}&include_adult=&api_key={TMDB_API_KEY}" # avoid adult content, but not work idk why
+    
+    url = f"https://api.themoviedb.org/3/search/tv?query={series_id}&include_adult=false&api_key={TMDB_API_KEY}"
+    
+    # response = requests.get(url)
+    # movie_data = response.json()
+    # print(movie_data["title"])
+    # url = f"https://api.themoviedb.org/3/movie/{movie_data['id']}?api_key={TMDB_API_KEY}"
+  
+  print(url)
+  response = requests.get(url)
+  # rep = requests.get(f"https://api.themoviedb.org/3/account/{account_id}")
+  movie_data = response.json()
+  if 'results' in movie_data:
+    # far more better but worst in some case
+    movie_data['results'] = list(filter(lambda result: result['adult'] == False and NotAdult(result), movie_data['results']))
   return movie_data
 
 # Fetch the movie data for the movie with the ID 12345
@@ -45,12 +88,16 @@ def GetUserGeneralInputMaybe():
   print(value)
   if str(value) != "None":
       # print(value)
-      if type(value) == str: movie_data = fetch_movie_data(str(value))
-      else: movie_data = fetch_movie_data(int(value))
+      if type(value) == str: 
+        movie_data = fetch_movie_data(str(value))
+        series_data = fetch_series_data(str(value))
+      else: 
+        movie_data = fetch_movie_data(int(value))
+        series_data = fetch_series_data(int(value))
   else:
       movie_data = fetch_movie_data(166426) # 878 # random.randint(1, 99999) # watching Pirate of the caribbean 5 instead of random
   # Render the template with the movie data
-  return render_template("moviedbTest.html", movie_data=movie_data)
+  return render_template("moviedbTest.html", movie_data=movie_data, series_data=series_data)
 
 @app.route("/watchMovie", methods=['POST','GET'])
 def WatchMovie():
@@ -62,13 +109,30 @@ def WatchMovie():
   video_url = f"https://api.themoviedb.org/3/movie/{id}/videos?api_key={TMDB_API_KEY}"
   video_data = requests.get(video_url).json()
   
-  watch_provider = f'https://api.themoviedb.org/3/movie/{id}/watch/providers?api_key={TMDB_API_KEY}'
-  watch_data = requests.get(watch_provider).json()
+  # watch_provider = f'https://api.themoviedb.org/3/movie/{id}/watch/providers?api_key={TMDB_API_KEY}'
+  # watch_data = requests.get(watch_provider).json()
   
   vidsrc_url = f"https://vidsrc.to/embed/movie/{movie_data['id']}"
   print(vidsrc_url)
   
-  return render_template("watchMovie.html", movie_data=movie_data, video_data=video_data, watch_data = watch_data, vid_url = vidsrc_url)
+  return render_template("watchMovie.html", movie_data=movie_data, video_data=video_data, vid_url = vidsrc_url)
+
+@app.route("/watchSeries", methods=['POST','GET'])
+def WatchSeries():
+  id = request.form.get("I clicked it")
+  
+  movie_data = fetch_series_data(id) # 878
+  
+  video_url = f"https://api.themoviedb.org/3/tv/{id}/videos?api_key={TMDB_API_KEY}"
+  video_data = requests.get(video_url).json()
+  
+  # watch_provider = f'https://api.themoviedb.org/3/tv/{id}/watch/providers?api_key={TMDB_API_KEY}'
+  # watch_data = requests.get(watch_provider).json()
+  
+  vidsrc_url = f"https://vidsrc.to/embed/tv/{movie_data['id']}"
+  print(vidsrc_url)
+  
+  return render_template("watchSeries.html",movie_data=movie_data, video_data=video_data, vid_url = vidsrc_url)
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
